e2b97d45bab4bb23f3c11c8af30c076a17e91526
sakethkreddy2000 <sakethkethireddy2000@gmaisdval.com>
2025-07-06 16:44:00 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
+
+@app.route("/")
+def hello():
+    REQUEST_COUNT.inc()
+    return "Hello from DevOps!"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
