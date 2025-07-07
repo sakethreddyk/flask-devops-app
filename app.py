c7e3c21ad1a2714643aaa353153e437d66a18a1b
sakethkreddy2000 <sakethkethireddy2000@gmaisdval.com>
2025-07-07 18:11:16 -0400

    Change from devops to sakethredyy

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
 @app.route("/")
 def hello():
     REQUEST_COUNT.inc()
-    return "Hello from DevOps!"
+    return "Hello from SakethReddy!"
 
 @app.route("/metrics")
 def metrics():
